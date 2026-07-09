2a655f75f42e60aad027dafdead5a4a83846cccd
Hemant Kumar <hemantkr2262004@gmail.com>
2026-07-09 12:20:31 +0530

    new code

--- Banking-App/src/main/java/com/example/Banking_App/controller/Accountcontroller.java
+++ Banking-App/src/main/java/com/example/Banking_App/controller/Accountcontroller.java
@@ -5,10 +5,9 @@ import com.example.Banking_App.dto.AccountDto;
 import com.example.Banking_App.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -23,4 +22,26 @@ public class Accountcontroller {
     public ResponseEntity<AccountDto>addAccount(@RequestBody AccountDto accountDto){
         return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED );
     }
+    // Get Account Rest API
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> getAccountbyId(@PathVariable Long id){
+        AccountDto accountDto = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
+    }
+  //Deposite rest API
+    @PutMapping("/{id}/deposit")
+    public ResponseEntity<AccountDto> deposit(@PathVariable Long id, @RequestBody Map<String,Double> request){
+        Double amount = request.get("amount");
+        AccountDto accountDto= accountService.deposit(id,amount);
+        return ResponseEntity.ok(accountDto);
+    }
 }
+
+
+
+
+
+
+
+
+
--- Banking-App/src/main/java/com/example/Banking_App/service/AccountService.java
+++ Banking-App/src/main/java/com/example/Banking_App/service/AccountService.java
@@ -4,5 +4,9 @@ import com.example.Banking_App.dto.AccountDto;
 
 public interface AccountService {
     AccountDto createAccount (AccountDto accountDto );
-    AccountDto getAccountById(Long iid);
+    AccountDto getAccountById(Long id);
+    AccountDto deposit(Long id, Double amount);
+    AccountDto  withdraw(Long id , Double amount);
+
+
 }
